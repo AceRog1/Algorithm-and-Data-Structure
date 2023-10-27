bedbe715eed8885a1d7495fd033a23a4832eaf34
Your NameAlejandro <andresrojas72727264@gmail.com>
2023-10-27 14:28:22 -0500

    Estructura inicial de la clase List y su clase derivada ForwardList

--- Lists/ForwardList.h
+++ Lists/ForwardList.h
@@ -5,9 +5,46 @@
 #ifndef ALGORITHMANDDATASTRUCTURE_FORWARDLIST_H
 #define ALGORITHMANDDATASTRUCTURE_FORWARDLIST_H
 
+#include "List.h"
 
-class ForwardList {
-
+template<typename T>
+class ForwardList : public List<T>{
+private:
+    struct Node{
+        T data;
+        Node* next;
+        Node(){
+            next = nullptr;
+        }
+        explicit Node(T value){
+            data = value;
+            next = nullptr;
+        }
+        void killself(){
+            next = nullptr;
+            delete this;
+        };
+    };
+private:
+    int length;
+    Node* head;
+public:
+    T front() override;
+    T back() override;
+    void push_front(T) override;
+    void push_back(T) override;
+    T pop_front() override;
+    T pop_back() override;
+    bool insert(T, int) override;
+    bool remove(int) override;
+    T& operator[](int) override;
+    bool is_empty() override;
+    int size() override;
+    void clear() override;
+    void sort() override;
+    bool is_sorted() override;
+    void reverse() override;
+    std::string name() override;
 };
 
 
--- Lists/List.h
+++ Lists/List.h
@@ -5,4 +5,28 @@
 #ifndef ALGORITHMANDDATASTRUCTURE_LIST_H
 #define ALGORITHMANDDATASTRUCTURE_LIST_H
 
+#include <iostream>
+#include <string>
+
+template<typename T>
+class List{
+public:
+    virtual T front() = 0;
+    virtual T back() = 0;
+    virtual void push_front(T) = 0;
+    virtual void push_back(T) = 0;
+    virtual T pop_front() = 0;
+    virtual T pop_back() = 0;
+    virtual bool insert(T, int) = 0;
+    virtual bool remove(int) = 0;
+    virtual T& operator[](int) = 0;
+    virtual bool is_empty() = 0;
+    virtual int size() = 0;
+    virtual void clear() = 0;
+    virtual void sort() = 0;
+    virtual bool is_sorted() = 0;
+    virtual void reverse() = 0;
+    virtual std::string name() = 0;
+};
+
 #endif //ALGORITHMANDDATASTRUCTURE_LIST_H
