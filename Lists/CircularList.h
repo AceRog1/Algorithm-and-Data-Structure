e2ee412ac70daa65e1ee7a7f56d13213fdd2cf8d
Your NameAlejandro <andresrojas72727264@gmail.com>
2023-10-27 14:38:51 -0500

    Estructura inicial del CircularList

--- Lists/CircularList.h
+++ Lists/CircularList.h
@@ -6,9 +6,53 @@
 #define ALGORITHMANDDATASTRUCTURE_CIRCULARLIST_H
 
 
-class CircularList {
+#include "List.h"
 
+template<typename T>
+class CircularList : public List<T>{
+private:
+    struct Node{
+        T data;
+        Node* next;
+        Node* prev;
+        Node(){
+            next = nullptr;
+            prev = nullptr;
+        }
+        explicit Node(T value){
+            data = value;
+            next = nullptr;
+            prev = nullptr;
+        }
+        void killself(){
+            next = nullptr;
+            prev = nullptr;
+            delete this;
+        };
+    };
+private:
+    int length;
+    Node* head;
+    Node* tail;
+public:
+    CircularList();
+    T front() override;
+    T back() override;
+    void push_front(T) override;
+    void push_back(T) override;
+    T pop_front() override;
+    T pop_back() override;
+    bool insert(T, int) override;
+    bool remove(int) override;
+    T& operator[](int) override;
+    bool is_empty() override;
+    int size() override;
+    void clear() override;
+    void sort() override;
+    bool is_sorted() override;
+    void reverse() override;
+    std::string name() override;
+    ~CircularList();
 };
-
 
 #endif //ALGORITHMANDDATASTRUCTURE_CIRCULARLIST_H
