de4ca70fa2ab54de9c118b2d1a782c43ae333dd6
Your NameAlejandro <andresrojas72727264@gmail.com>
2023-11-06 23:52:35 -0500

    Implementacion del name y otras caracteristicas.

--- Lists/ForwardList.cpp
+++ Lists/ForwardList.cpp
@@ -3,6 +3,9 @@
 //
 
 #include "ForwardList.h"
+#include <string>
+
+using namespace std;
 
 template<typename T>
 ForwardList<T>::ForwardList(){
@@ -27,5 +30,138 @@ T ForwardList<T>::back(){
 template<typename T>
 void ForwardList<T>::push_front(T value){
     Node* newNode = new Node(value);
+    if (size() == 0){
+        head = newNode;
+    } else {
+        newNode->next = head;
+        head = newNode;
+    }
+    length++;
+}
 
+template<typename T>
+void ForwardList<T>::push_back(T value){
+    Node* newNode = new Node(value);
+    if (size() == 0){
+        head = newNode;
+    } else {
+        Node* temp = head;
+        for(int i = length; i < size(); i++)
+            temp = temp->next;
+        temp->next = newNode;
+    }
+    length++;
+}
+
+template<typename T>
+T ForwardList<T>::pop_front(){
+    if (size() == 0){
+        throw runtime_error("No elements in the list");
+    } else {
+        Node* popNode = head;
+        head = popNode->next;
+        T value = popNode->data;
+        popNode->killself();
+        length--;
+        return value;
+    }
+}
+
+template<typename T>
+T ForwardList<T>::pop_back(){
+    if (size() == 0){
+        throw runtime_error("No elemntes in the list");
+    } else if (size() == 1){
+        Node* popNode = head;
+        T value = popNode->data;
+        popNode->killself();
+        length--;
+        return value;
+    } else {
+        Node* popNode = head;
+        for(int i = 0; i < size()-1; i++)
+            popNode = popNode->next;
+        Node* lastNode = popNode;
+        popNode->next;
+        lastNode->next = nullptr;
+        T value = popNode->data;
+        popNode->killself();
+        length--;
+        return value;
+    }
+}
+
+template<typename T>
+bool ForwardList<T>::insert(T, int){
+    //TODO
+}
+
+template<typename T>
+bool ForwardList<T>::remove(int){
+    //TODO
+}
+
+template<typename T>
+T& ForwardList<T>::operator[](int){
+    //TODO
+}
+
+template<typename T>
+bool ForwardList<T>::is_empty(){
+    if(size() == 0)
+        return true;
+    else
+        return false;
+}
+
+template<typename T>
+int ForwardList<T>::size(){
+    return length;
+}
+
+template<typename T>
+void ForwardList<T>::clear(){
+    for(int i = 0; i < size(); i++){
+        pop_front();
+    }
+}
+
+template<typename T>
+void ForwardList<T>::sort(){
+    //TODO
+}
+
+template<typename T>
+bool ForwardList<T>::is_sorted(){
+    //TODO
+}
+
+template<typename T>
+void ForwardList<T>::reverse(){
+    //TODO
+}
+
+template<typename T>
+string ForwardList<T>::name(){
+    if (is_empty()){
+        throw runtime_error("No elements in the list");
+    } else {
+        Node* temp = head;
+        string nameString;
+        for(int i = 0; i < size(); i++){
+            string part = to_string(temp->data);
+            if (i+1 == size()){
+                nameString += part;
+            } else {
+                nameString += part + ", ";
+            }
+            temp = temp->next;
+        }
+        return nameString;
+    }
+}
+
+template<typename T>
+ForwardList<T>::~ForwardList(){
+    clear();
 }
--- main.cpp
+++ main.cpp
@@ -2,11 +2,26 @@
 // Created by alejandro on 21/10/2023.
 //
 
-#include <iostream>
+//#include <iostream>
+//#include "Lists/List.h"
+#include "Lists/ForwardList.h"
+#include "Lists/ForwardList.cpp"
 
 int main(){
 
-    std::cout << "Hello World!" << std::endl;
+    ForwardList<int> lista;
+    lista.push_front(1);
+    lista.push_front(2);
+    lista.push_front(3);
+    lista.push_front(4);
+    //std::cout << lista.size() << std::endl;
+    //lista.push_back(111);
+    //std::cout << lista.size() << std::endl;
+    lista.pop_back();
+    lista.pop_back();
+    lista.pop_back();
+    //lista.pop_back();
+    std::cout << lista.name() << std::endl;
 
     return 0;
 }
