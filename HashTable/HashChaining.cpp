0060874efaf5fa1b365c8936d36f787b63485426
acerog <andresrojas72727264@gmail.com>
2024-11-10 12:26:49 -0500

    Creacion de declaracion de funciones del HashChaining.h

--- HashTable/HashChaining.cpp
+++ HashTable/HashChaining.cpp
@@ -3,3 +3,65 @@
 //
 
 #include "HashChaining.h"
+
+using namespace std;
+
+template<typename TK, typename TV>
+HashChaining<TK, TV>::HashChaining(int cap) {
+    // TODO
+}
+
+template<typename TK, typename TV>
+size_t HashChaining<TK, TV>::size() {
+    // TODO
+}
+
+template<typename TK, typename TV>
+int HashChaining<TK, TV>::max_size() {
+    // TODO
+}
+
+template<typename TK, typename TV>
+void HashChaining<TK, TV>::insert(TK key, TV value) {
+    // TODO
+}
+
+template<typename TK, typename TV>
+bool HashChaining<TK, TV>::remove(TK key) {
+    // TODO
+}
+
+template<typename TK, typename TV>
+pair<TK, TV> HashChaining<TK, TV>::search(TK key) {
+    // TODO
+}
+
+template<typename TK, typename TV>
+void HashChaining<TK, TV>::clear() {
+    // TODO
+}
+
+template<typename TK, typename TV>
+bool HashChaining<TK, TV>::empty() {
+    // TODO
+}
+
+template<typename TK, typename TV>
+TV& HashChaining<TK, TV>::operator[](TK key) {
+    // TODO
+}
+
+template<typename TK, typename TV>
+pair<TK, TV> HashChaining<TK, TV>::extract(TK key) {
+    // TODO
+}
+
+template<typename TK, typename TV>
+HashChaining<TK, TV>::~HashChaining() {
+    // TODO
+}
+
+template<typename TK, typename TV>
+void HashChaining<TK, TV>::rehashing() {
+    // TODO
+}
--- HashTable/HashChaining.h
+++ HashTable/HashChaining.h
@@ -5,10 +5,38 @@
 #ifndef ALGORITHMANDDATASTRUCTURE_HASHCHAINING_H
 #define ALGORITHMANDDATASTRUCTURE_HASHCHAINING_H
 
+#include <iostream>
+#include <utility>
+#include <string>
+#include "List.h"
+
+const double MAXFILLFACTOR = 0.5;
+const int MAXCOLISION = 3;
+
 template<typename TK, typename TV>
 class HashChaining {
-
+private:
+    size_t length{};
+    int capacity{};
+    ListNode<std::pair<TK, TV>>** hash_table;
+public:
+    explicit HashChaining(int cap = 13);
+    size_t size();
+    int max_size();
+    void insert(TK key, TV value);
+    bool remove(TK key);
+    std::pair<TK, TV> search(TK key);
+    void clear();
+    bool empty();
+    TV& operator[](TK key);
+    std::pair<TK, TV> extract(TK key);
+    ~HashChaining();
+private:
+    void rehashing();
+    size_t hashFunc(TK key){
+        if (key == 0) return 0;
+        else return key%capacity;
+    }
 };
-
 
 #endif //ALGORITHMANDDATASTRUCTURE_HASHCHAINING_H
